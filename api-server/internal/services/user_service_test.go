c49206bb763e0d1777ad3e530443607b20b521fc
yamao-sys <yamaopanku@gmail.com>
2025-04-24 11:29:45 +0900

    User Service Testの追加

---
+++ api-server/internal/services/user_service_test.go
@@ -0,0 +1,90 @@
+package services
+
+import (
+	"apps/api"
+	"apps/internal/models"
+	"fmt"
+	"testing"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+type TestUserServiceSuite struct {
+	WithDBSuite
+}
+
+var testUserService UserService
+
+func (s *TestUserServiceSuite) SetupTest() {
+	s.SetDBCon()
+
+	testUserService = NewUserService(DBCon)
+}
+
+func (s *TestUserServiceSuite) TearDownTest() {
+	s.CloseDB()
+}
+
+func (s *TestUserServiceSuite) TestValidateSignUp_SuccessRequiredFields() {
+	requestParams := api.PostUsersSignUpJSONRequestBody{
+		Name: "name",
+		Email: "test@example.com",
+		Password: "Password",
+	}
+
+	result := testUserService.ValidateSignUp(ctx, requestParams)
+
+	assert.Nil(s.T(), result)
+}
+
+func (s *TestUserServiceSuite) TestValidateSignUp_ValidationErrorRequiredFields() {
+	requestParams := api.PostUsersSignUpJSONRequestBody{
+		Name: "",
+		Email: "",
+		Password: "",
+	}
+
+	result := testUserService.ValidateSignUp(ctx, requestParams)
+
+	assert.NotNil(s.T(), result)
+	if errors, ok := result.(validation.Errors); ok {
+		for field, err := range errors {
+			message := err.Error()
+			switch field {
+			case "name":
+				assert.Equal(s.T(), "ユーザ名は必須入力です。", message)
+			case "email":
+				assert.Equal(s.T(), "Emailは必須入力です。", message)
+			case "password":
+				assert.Equal(s.T(), "パスワードは必須入力です。", message)
+			}
+		}
+	}
+}
+
+func (s *TestUserServiceSuite) TestSignUp_SuccessRequiredFields() {
+	requestParams := api.PostUsersSignUpJSONRequestBody{
+		Name: "name",
+		Email: "test@example.com",
+		Password: "Password",
+	}
+
+	result := testUserService.SignUp(ctx, requestParams)
+
+	assert.Nil(s.T(), result)
+
+	// NOTE: ユーザが作成されていることを確認
+	var exists bool
+	err := DBCon.Model(&models.User{}).Select("count(*) > 0").Where("email = ?", "test@example.com").Find(&exists).Error
+	if err != nil {
+		fmt.Println("err", err)
+	}
+	assert.True(s.T(), exists)
+}
+
+func TestUserService(t *testing.T) {
+	// テストスイートを実行
+	suite.Run(t, new(TestUserServiceSuite))
+}
