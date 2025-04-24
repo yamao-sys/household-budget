7f179904e33bd7e2b02c19141604914a413c3815
yamao-sys <yamaopanku@gmail.com>
2025-04-24 10:26:03 +0900

    APIのエントリポイントの追加

---
+++ api-server/cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	// NOTE: デプロイ先の環境はSecret Managerで環境変数を管理する
+	if os.Getenv("APP_ENV") != "production" {
+		loadEnv()
+	}
+
+	// NOTE: Handlerをルーティングに追加
+	// e := businessmiddlewares.ApplyMiddlewares(echo.New())
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, Household Budget!")
+	})
+
+	if err := e.Start(":" + os.Getenv("SERVER_PORT")); err != nil && err != http.ErrServerClosed {
+		e.Logger.Errorf("Echo server error: %v", err)
+	}
+}
+
+func loadEnv() {
+	envFilePath := os.Getenv("ENV_FILE_PATH")
+	if envFilePath == "" {
+		envFilePath = ".env"
+	}
+	godotenv.Load(envFilePath)
+}
