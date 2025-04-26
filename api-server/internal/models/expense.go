abf741e87d7ebb38c30e2b40fbd2e05892c5817d
yamao-sys <yamaopanku@gmail.com>
2025-04-26 15:54:22 +0900

    expensesテーブルにdescriptionカラムを追加したため、ModelとFactoryの修正

--- api-server/internal/models/expense.go
+++ api-server/internal/models/expense.go
@@ -7,6 +7,7 @@ type Expense struct {
 	Amount      int `gorm:"not null" validate:"required"`
 	Category     int `gorm:"not null" validate:"required"`
 	PaidAt  time.Time `gorm:"not null;type:date;column:paid_at" validate:"required"`
+	Description string `gorm:"not null" validate:"required"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	UserID  int    `gorm:"not null" json:"user_id"`
--- api-server/test/factories/expense.go
+++ api-server/test/factories/expense.go
@@ -12,6 +12,7 @@ var ExpenseFactory = factory.NewFactory(
 	&models.Expense{
 		Amount: randomdata.Number(10000),
 		Category: randomdata.Number(1),
+		Description: randomdata.RandStringRunes(100),
 	},
 ).Attr("PaidAt", func(args factory.Args) (interface{}, error) {
 	date := time.Date(2025, 4, 1, 0, 0, 0, 0, time.Local)
