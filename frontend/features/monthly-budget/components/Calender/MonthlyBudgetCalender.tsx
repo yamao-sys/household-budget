f514f8e4f1a1a0907f337beeec41bfc8e2642ece
yamao-sys <yamaopanku@gmail.com>
2025-04-29 18:26:06 +0900

    支出一覧カレンダーから選択日付の支出登録を可能に

--- frontend/features/monthly-budget/components/Calender/MonthlyBudgetCalender.tsx
+++ frontend/features/monthly-budget/components/Calender/MonthlyBudgetCalender.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 // NOTE: FullCalendarコンポーネント。
 import FullCalendar from "@fullcalendar/react";
 // NOTE: FullCalendarで月表示を可能にするモジュール。
@@ -9,8 +9,22 @@ import interactionPlugin, { type DateClickArg } from "@fullcalendar/interaction"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { DatesSetArg } from "@fullcalendar/core";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
-import type { Expense, TotalAmountLists } from "~/types";
-import { getExpenses, getTotalAmounts } from "~/apis/expenses.api";
+import type { StoreExpenseInput, Expense, TotalAmountLists, StoreExpenseValidationError } from "~/types";
+import { getExpenses, getTotalAmounts, postCreateExpense } from "~/apis/expenses.api";
+import BaseFormInput from "~/components/BaseFormInput";
+import BaseButton from "~/components/BaseButton";
+
+const INITIAL_STORE_EXPENSE_INPUT = {
+  paidAt: new Date(),
+  amount: 0,
+  category: 1,
+  description: "",
+};
+const INITIAL_VALIDATION_ERRORS = {
+  amount: [],
+  category: [],
+  description: [],
+};
 
 export const MonthlyBudgetCalender: React.FC = () => {
   /**
@@ -24,6 +38,18 @@ export const MonthlyBudgetCalender: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState("");
   const [selectedDateExpenses, setSelectedDateExpenses] = useState<Expense[]>([]);
   const [currentMonthDate, setCurrentMonthDate] = useState<Date>(new Date());
+
+  const [storeExpenseInput, setStoreExpenseInput] = useState<StoreExpenseInput>(INITIAL_STORE_EXPENSE_INPUT);
+  const updateStoreExpenseInput = useCallback((params: Partial<StoreExpenseInput>) => {
+    setStoreExpenseInput((prev: StoreExpenseInput) => ({ ...prev, ...params }));
+  }, []);
+  const setStoreExpenseTextInput = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      updateStoreExpenseInput({ [e.target.name]: e.target.value });
+    },
+    [updateStoreExpenseInput],
+  );
+  const [validationErrors, setValidationErrors] = useState<StoreExpenseValidationError>(INITIAL_VALIDATION_ERRORS);
 
   // NOTE: 月が変更された時の処理
   const handleDatesSet = async (arg: DatesSetArg) => {
@@ -50,6 +76,36 @@ export const MonthlyBudgetCalender: React.FC = () => {
     setSelectedDateExpenses(await getExpenses(date, date));
     setSelectedDate(date);
     setInView(true);
+    setStoreExpenseInput((prev: StoreExpenseInput) => ({ ...prev, ...{ paidAt: arg.date } }));
+  };
+
+  const handleCreateExpense = async () => {
+    setValidationErrors(INITIAL_VALIDATION_ERRORS);
+
+    const { expense, errors } = await postCreateExpense(storeExpenseInput);
+    if (Object.keys(errors).length > 0) {
+      setValidationErrors(errors);
+      return;
+    }
+
+    window.alert("支出を登録しました");
+
+    // NOTE: 選択月の支出に反映する
+    const otherDateEvents = events.filter((event) => String(event.date) !== selectedDate);
+    const currentDateEvent = events.find((event) => String(event.date) === selectedDate);
+    const newTotalAmount = currentDateEvent?.extendProps.totalAmount ?? 0;
+    setEvents([
+      ...otherDateEvents,
+      {
+        date: expense.paidAt,
+        extendProps: {
+          type: "expense",
+          totalAmount: newTotalAmount + expense.amount,
+        },
+      },
+    ]);
+    setInView(false);
+    setStoreExpenseInput(INITIAL_STORE_EXPENSE_INPUT);
   };
 
   const formElement = (
@@ -87,14 +143,36 @@ export const MonthlyBudgetCalender: React.FC = () => {
         </div>
 
         {/* 入力フォーム */}
-        <form className='flex flex-col gap-2 mb-4 w-full'>
-          <input name='amount' type='number' placeholder='金額' required className='border px-2 py-1 rounded' />
-          <input name='usage' type='text' placeholder='適用' required className='border px-2 py-1 rounded' />
-          <input name='category' type='text' placeholder='カテゴリ' required className='border px-2 py-1 rounded' />
-          <button type='submit' className='mx-auto w-50 bg-blue-500 text-white px-4 py-1 rounded'>
-            追加
-          </button>
-        </form>
+        <div className='flex flex-col gap-2 mb-4 w-full'>
+          <div className='mb-2'>
+            <BaseFormInput
+              id='amount'
+              label='金額'
+              name='amount'
+              type='number'
+              value={storeExpenseInput.amount || ""}
+              onChange={setStoreExpenseTextInput}
+              validationErrorMessages={validationErrors.amount ?? []}
+            />
+          </div>
+          <div className='mb-2'>
+            <BaseFormInput
+              id='description'
+              label='適用'
+              name='description'
+              type='text'
+              value={storeExpenseInput.description}
+              onChange={setStoreExpenseTextInput}
+              validationErrorMessages={validationErrors.description ?? []}
+            />
+          </div>
+
+          <div className='w-full flex justify-center'>
+            <div className='mt-16'>
+              <BaseButton borderColor='border-green-500' bgColor='bg-green-500' label='登録する' onClick={handleCreateExpense} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
