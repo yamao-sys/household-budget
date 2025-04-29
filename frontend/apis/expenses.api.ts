60ac8923fb5ea9601fef2d1aac1f6ea0210f4c85
yamao-sys <yamaopanku@gmail.com>
2025-04-29 17:00:50 +0900

    支出一覧カレンダーの選択月の日ごとの支出額APIクライアント作成

---
+++ frontend/apis/expenses.api.ts
@@ -0,0 +1,48 @@
+import createClient from "openapi-fetch";
+import type { operations, paths } from "./generated/apiSchema";
+import { getRequestHeaders } from "./csrf.api";
+import type { Expense } from "~/types";
+
+const client = createClient<paths>({
+  baseUrl: `${import.meta.env.VITE_API_ENDPOINT_URI}/`,
+  credentials: "include",
+});
+
+// export async function getExpenses(fromDate: string, toDate: string) {
+//   const params: operations["get-expenses"]["parameters"] = { query: {} };
+//   if (!!fromDate) {
+//     params.query = { ...params.query, fromDate };
+//   }
+//   if (!!toDate) {
+//     params.query = { ...params.query, toDate };
+//   }
+
+//   const { data } = await client.GET("/expenses", {
+//     ...(await getRequestHeaders()),
+//     ...params,
+//   });
+
+//   const emptyExpenses: Expense[] = [];
+
+//   return data?.expenses ?? emptyExpenses;
+// }
+
+export async function getTotalAmounts(fromDate: string, toDate: string) {
+  const params: operations["get-expenses-total-amounts"]["parameters"] = { query: { fromDate: "", toDate: "" } };
+  if (!!fromDate) {
+    params.query = { ...params.query, fromDate };
+  }
+  if (!!toDate) {
+    params.query = { ...params.query, toDate };
+  }
+
+  const { data } = await client.GET("/expenses/totalAmounts", {
+    ...(await getRequestHeaders()),
+    params,
+  });
+  if (!data) {
+    throw new Error();
+  }
+
+  return data.totalAmounts;
+}
