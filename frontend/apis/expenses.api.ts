3b06f40230858128ede6a7f01ee62c79d88aaa60
yamao-sys <yamaopanku@gmail.com>
2025-04-30 22:14:50 +0900

    FrontendにGET /expenses/categoryTotalAmountsのAPIクライアント作成

--- frontend/apis/expenses.api.ts
+++ frontend/apis/expenses.api.ts
@@ -47,6 +47,26 @@ export async function getTotalAmounts(fromDate: string, toDate: string) {
   return data.totalAmounts;
 }
 
+export async function getCategoryTotalAmounts(fromDate: string, toDate: string) {
+  const params: operations["get-expenses-category-total-amounts"]["parameters"] = { query: { fromDate: "", toDate: "" } };
+  if (!!fromDate) {
+    params.query = { ...params.query, fromDate };
+  }
+  if (!!toDate) {
+    params.query = { ...params.query, toDate };
+  }
+
+  const { data } = await client.GET("/expenses/categoryTotalAmounts", {
+    ...(await getRequestHeaders()),
+    params,
+  });
+  if (!data) {
+    throw new Error();
+  }
+
+  return data.totalAmounts;
+}
+
 export async function postCreateExpense(input: StoreExpenseInput) {
   const { data } = await client.POST("/expenses", {
     ...(await getRequestHeaders()),
