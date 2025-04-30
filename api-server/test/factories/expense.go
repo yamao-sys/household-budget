a244f24bbf961fe354c4fddb3bac3b7dca014c66
yamao-sys <yamaopanku@gmail.com>
2025-04-30 19:45:03 +0900

    expenseのfactoryの調整

--- api-server/test/factories/expense.go
+++ api-server/test/factories/expense.go
@@ -9,12 +9,14 @@ import (
 )
 
 var ExpenseFactory = factory.NewFactory(
-	&models.Expense{
-		Amount: randomdata.Number(10000),
-		Category: models.Category(randomdata.Number(0, 8)),
-		Description: randomdata.RandStringRunes(100),
-	},
-).Attr("PaidAt", func(args factory.Args) (interface{}, error) {
+	&models.Expense{},
+).Attr("Amount", func(args factory.Args) (interface{}, error) {
+	return randomdata.Number(10000), nil
+}).Attr("Category", func(args factory.Args) (interface{}, error) {
+	return models.Category(randomdata.Number(1, 9)), nil
+}).Attr("Description", func(args factory.Args) (interface{}, error) {
+	return randomdata.RandStringRunes(100), nil
+}).Attr("PaidAt", func(args factory.Args) (interface{}, error) {
 	date := time.Date(2025, 4, 1, 0, 0, 0, 0, time.Local)
 	return date, nil
 }).SubFactory("User", UserFactory)
