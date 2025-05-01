629df0de6228e0162a2b870a7edd97e3234b8c01
yamao-sys <yamaopanku@gmail.com>
2025-05-01 22:23:29 +0900

    Income Validatorの追加

---
+++ api-server/internal/validators/income_validator.go
@@ -0,0 +1,20 @@
+package validators
+
+import (
+	"apps/api"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
+
+func ValidateIncome(input *api.PostIncomesJSONRequestBody) error {
+	return validation.ValidateStruct(input,
+		validation.Field(
+			&input.Amount,
+			validation.Required.Error("金額は必須入力です。"),
+		),
+		validation.Field(
+			&input.ClientName,
+			validation.Required.Error("顧客名は必須入力です。"),
+		),
+	)
+}
