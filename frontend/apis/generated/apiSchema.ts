c295c56166d0b79819e88289e22577f3cae392a9
yamao-sys <yamaopanku@gmail.com>
2025-05-02 03:56:06 +0900

    apiSchemaを最新に

--- frontend/apis/generated/apiSchema.ts
+++ frontend/apis/generated/apiSchema.ts
@@ -174,6 +174,23 @@ export interface paths {
     patch?: never;
     trace?: never;
   };
+  "/incomes/clientTotalAmounts": {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    /** Get Incomes Client TotalAmounts */
+    get: operations["get-incomes-client-total-amounts"];
+    put?: never;
+    post?: never;
+    delete?: never;
+    options?: never;
+    head?: never;
+    patch?: never;
+    trace?: never;
+  };
 }
 export type webhooks = Record<string, never>;
 export interface components {
@@ -253,6 +270,14 @@ export interface components {
     /** IncomeLists */
     IncomeLists: {
       incomes: components["schemas"]["Income"][];
+    };
+    /**
+     * ClientTotalAmountLists
+     * @description Total Amount Lists
+     */
+    ClientTotalAmountLists: {
+      clientName: string;
+      totalAmount: number;
     };
   };
   responses: {
@@ -361,6 +386,17 @@ export interface components {
       };
       content: {
         "application/json": components["schemas"]["IncomeLists"];
+      };
+    };
+    /** @description Client Total Amount Lists Response */
+    ClientTotalAmountListsResponse: {
+      headers: {
+        [name: string]: unknown;
+      };
+      content: {
+        "application/json": {
+          totalAmounts: components["schemas"]["ClientTotalAmountLists"][];
+        };
       };
     };
     /** @description Internal Server Error Response */
@@ -611,6 +647,23 @@ export interface operations {
     requestBody?: never;
     responses: {
       200: components["responses"]["TotalAmountListsResponse"];
+    };
+  };
+  "get-incomes-client-total-amounts": {
+    parameters: {
+      query: {
+        /** @description 取得対象の日付FROM */
+        fromDate: string;
+        /** @description 取得対象の日付TO */
+        toDate: string;
+      };
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    requestBody?: never;
+    responses: {
+      200: components["responses"]["ClientTotalAmountListsResponse"];
     };
   };
 }
