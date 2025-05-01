6e267d117f65dfb832a3ce13b11234902fa21486
yamao-sys <yamaopanku@gmail.com>
2025-05-02 03:57:20 +0900

    ClientTotalAmountListsを月の収支詳細画面で表示

--- frontend/features/monthly-budget/components/Detail/MonthlyBudgetDetail/index.tsx
+++ frontend/features/monthly-budget/components/Detail/MonthlyBudgetDetail/index.tsx
@@ -1,18 +1,20 @@
 import { EXPENSE_CATEGORY } from "~/const/expense";
 import { getMonthLocaleString } from "~/lib/date";
-import type { CategoryTotalAmountLists } from "~/types";
+import type { CategoryTotalAmountLists, ClientTotalAmountLists } from "~/types";
 
 type Props = {
   monthDate: Date;
   categoryTotalAmounts: CategoryTotalAmountLists;
+  clientTotalAmounts: ClientTotalAmountLists;
 };
 
-export const MonthlyBudgetDetail: React.FC<Props> = ({ monthDate, categoryTotalAmounts }: Props) => {
+export const MonthlyBudgetDetail: React.FC<Props> = ({ monthDate, categoryTotalAmounts, clientTotalAmounts }: Props) => {
   return (
     <div className='mx-auto mt-4'>
-      <h2 className='text-2xl text-center font-bold mb-4'>{getMonthLocaleString(monthDate)}の支出詳細</h2>
+      <h2 className='text-2xl text-center font-bold mb-4'>{getMonthLocaleString(monthDate)}の収支詳細</h2>
 
       <div className='mb-4'>
+        <h3 className='text-xl text-center font-bold mb-4'>支出</h3>
         <table className='w-full table-fixed border border-gray-300 text-sm mb-4'>
           <thead className='sticky top-0 bg-white z-10 shadow-[0px_1px_0px_0px_rgba(209,213,219,1)]'>
             <tr>
@@ -33,6 +35,32 @@ export const MonthlyBudgetDetail: React.FC<Props> = ({ monthDate, categoryTotalA
               <td className='w-1/2 py-2 px-2 border border-gray-300 font-bold'>合計</td>
               <td className='w-1/2 py-2 px-2 border border-gray-300 font-bold'>
                 ¥{categoryTotalAmounts.reduce((acc, totalAmounts) => acc + totalAmounts.totalAmount, 0).toLocaleString()}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div className='mb-4'>
+        <h3 className='text-xl text-center font-bold mb-4'>収入</h3>
+        <table className='w-full table-fixed border border-gray-300 text-sm mb-4'>
+          <thead className='sticky top-0 bg-white z-10 shadow-[0px_1px_0px_0px_rgba(209,213,219,1)]'>
+            <tr>
+              <th className='w-1/2 text-left py-2 px-2 border border-gray-300'>顧客名</th>
+              <th className='w-1/2 text-left py-2 px-2 border border-gray-300'>支出合計額</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientTotalAmounts.map(({ clientName, totalAmount }, idx) => (
+              <tr key={idx}>
+                <td className='w-1/2 py-2 px-2 border border-gray-300'>{clientName}</td>
+                <td className='w-1/2 py-2 px-2 border border-gray-300'>¥{totalAmount.toLocaleString()}</td>
+              </tr>
+            ))}
+            <tr>
+              <td className='w-1/2 py-2 px-2 border border-gray-300 font-bold'>合計</td>
+              <td className='w-1/2 py-2 px-2 border border-gray-300 font-bold'>
+                ¥{clientTotalAmounts.reduce((acc, totalAmounts) => acc + totalAmounts.totalAmount, 0).toLocaleString()}
               </td>
             </tr>
           </tbody>
