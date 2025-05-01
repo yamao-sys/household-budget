5d94f1099ed6d389fc8026be54b23fdf9da31064
yamao-sys <yamaopanku@gmail.com>
2025-05-01 17:37:10 +0900

    MonthlyBudgetCalenderのロジックをカスタムフック化

--- frontend/features/monthly-budget/components/Calender/MonthlyBudgetCalender/index.tsx
+++ frontend/features/monthly-budget/components/Calender/MonthlyBudgetCalender/index.tsx
@@ -1,16 +1,13 @@
-import React, { useCallback, useMemo, useState } from "react";
+import React from "react";
 // NOTE: FullCalendarコンポーネント。
 import FullCalendar from "@fullcalendar/react";
 // NOTE: FullCalendarで月表示を可能にするモジュール。
 import dayGridPlugin from "@fullcalendar/daygrid";
 // NOTE: FullCalendarで日付や時間が選択できるようになるモジュール。
-import interactionPlugin, { type DateClickArg } from "@fullcalendar/interaction";
+import interactionPlugin from "@fullcalendar/interaction";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import type { DatesSetArg } from "@fullcalendar/core";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
-import type { StoreExpenseInput, Expense, TotalAmountLists, StoreExpenseValidationError } from "~/types";
-import { getExpenses, getTotalAmounts, postCreateExpense } from "~/apis/expenses.api";
 import BaseFormInput from "~/components/BaseFormInput";
 import BaseButton from "~/components/BaseButton";
 import { EXPENSE_CATEGORY } from "~/const/expense";
@@ -18,18 +15,7 @@ import BaseFormSelect from "~/components/BaseFormSelect";
 import { Link } from "react-router";
 import { NAVIGATION_PAGE_LIST } from "~/app/routes";
 import { getMonthString } from "~/lib/date";
-
-const INITIAL_STORE_EXPENSE_INPUT = {
-  paidAt: new Date(),
-  amount: 0,
-  category: 0,
-  description: "",
-};
-const INITIAL_VALIDATION_ERRORS = {
-  amount: [],
-  category: [],
-  description: [],
-};
+import { useMonthlyBudgetCalender } from "~/features/monthly-budget/hooks/useMonthlyBudgetCalender";
 
 export const MonthlyBudgetCalender: React.FC = () => {
   /**
@@ -38,103 +24,33 @@ export const MonthlyBudgetCalender: React.FC = () => {
    */
   const ref = React.createRef<any>();
 
-  const [events, setEvents] = useState<TotalAmountLists>([]);
-  const [inView, setInView] = useState(false);
-  const [selectedDate, setSelectedDate] = useState("");
-  const [selectedDateExpenses, setSelectedDateExpenses] = useState<Expense[]>([]);
-  const [currentMonthDate, setCurrentMonthDate] = useState<Date>(new Date());
+  const {
+    currentMonthDate,
+    summary,
+    handleDatesSet,
+    handleDateClick,
+    events,
 
-  const [storeExpenseInput, setStoreExpenseInput] = useState<StoreExpenseInput>(INITIAL_STORE_EXPENSE_INPUT);
-  const updateStoreExpenseInput = useCallback((params: Partial<StoreExpenseInput>) => {
-    setStoreExpenseInput((prev: StoreExpenseInput) => ({ ...prev, ...params }));
-  }, []);
-  const setStoreExpenseTextInput = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      updateStoreExpenseInput({ [e.target.name]: e.target.value });
-    },
-    [updateStoreExpenseInput],
-  );
-  const setStoreExpenseSelectInput = useCallback(
-    (e: React.ChangeEvent<HTMLSelectElement>) => {
-      updateStoreExpenseInput({ [e.target.name]: e.target.value });
-    },
-    [updateStoreExpenseInput],
-  );
-
-  const [validationErrors, setValidationErrors] = useState<StoreExpenseValidationError>(INITIAL_VALIDATION_ERRORS);
-
-  // NOTE: 月が変更された時の処理
-  const handleDatesSet = async (arg: DatesSetArg) => {
-    const selectedMonthBeginningDate = arg.view.currentStart;
-    const currentEnd = arg.view.currentEnd;
-    currentEnd.setDate(currentEnd.getDate() - 1);
-    const selectedMonthEndDate = currentEnd;
-
-    // TODO: ここをTanstack Query等を使用してキャッシュする
-    const fetchedTotalAmounts = await getTotalAmounts(
-      selectedMonthBeginningDate.toLocaleDateString("ja-jp", { year: "numeric", month: "2-digit", day: "2-digit" }).replaceAll("/", "-"),
-      selectedMonthEndDate.toLocaleDateString("ja-jp", { year: "numeric", month: "2-digit", day: "2-digit" }).replaceAll("/", "-"),
-    );
-    setCurrentMonthDate(selectedMonthBeginningDate);
-    setEvents(fetchedTotalAmounts);
-  };
-
-  // NOTE: 日が選択された時の処理
-  const handleDateClick = async (arg: DateClickArg) => {
-    // NOTE: 選択月以外の日付のクリックは無効にする
-    if (arg.date.getMonth() !== currentMonthDate.getMonth()) return;
-
-    const date = arg.date.toLocaleDateString("ja-jp", { year: "numeric", month: "2-digit", day: "2-digit" }).replaceAll("/", "-");
-    setSelectedDateExpenses(await getExpenses(date, date));
-    setSelectedDate(date);
-    setInView(true);
-    setStoreExpenseInput((prev: StoreExpenseInput) => ({ ...prev, ...{ paidAt: arg.date } }));
-  };
-
-  const handleCreateExpense = async () => {
-    setValidationErrors(INITIAL_VALIDATION_ERRORS);
-
-    const { expense, errors } = await postCreateExpense(storeExpenseInput);
-    if (Object.keys(errors).length > 0) {
-      setValidationErrors(errors);
-      return;
-    }
-
-    window.alert("支出を登録しました");
-
-    // NOTE: 選択月の支出に反映する
-    const otherDateEvents = events.filter((event) => String(event.date) !== selectedDate);
-    const currentDateEvent = events.find((event) => String(event.date) === selectedDate);
-    const newTotalAmount = currentDateEvent?.extendProps.totalAmount ?? 0;
-    setEvents([
-      ...otherDateEvents,
-      {
-        date: expense.paidAt,
-        extendProps: {
-          type: "expense",
-          totalAmount: newTotalAmount + expense.amount,
-        },
-      },
-    ]);
-    setInView(false);
-    setStoreExpenseInput(INITIAL_STORE_EXPENSE_INPUT);
-  };
+    dialog,
+  } = useMonthlyBudgetCalender();
 
   const formElement = (
     <div
       role='dialog'
       aria-modal='true'
-      className={inView ? "opacity-100 visible fixed top-1/8 left-1/4 font-bold bg-white w-1/2 flex justify-center items-center z-50" : "hidden"}
+      className={
+        dialog.inView ? "opacity-100 visible fixed top-1/8 left-1/4 font-bold bg-white w-1/2 flex justify-center items-center z-50" : "hidden"
+      }
     >
       <div className='bg-white p-4 rounded shadow-lg w-full max-h-[90vh] overflow-hidden flex flex-col relative'>
         <button
-          onClick={() => setInView(false)}
+          onClick={() => dialog.setInView(false)}
           className='absolute top-3 right-3 text-gray-500 hover:text-gray-700 text-xl border px-1'
           aria-label='閉じる'
         >
           <FontAwesomeIcon icon={faTimes} />
         </button>
-        <h2 className='text-xl text-center font-bold mb-4'>{selectedDate} の支出</h2>
+        <h2 className='text-xl text-center font-bold mb-4'>{dialog.selectedDate} の支出</h2>
 
         {/* 一覧表示 */}
         <div className='overflow-y-auto mb-4 border rounded max-h-50'>
@@ -147,7 +63,7 @@ export const MonthlyBudgetCalender: React.FC = () => {
               </tr>
             </thead>
             <tbody>
-              {selectedDateExpenses.map((expense, idx) => (
+              {dialog.selectedDateExpenses.map((expense, idx) => (
                 <tr key={idx}>
                   <td className='w-1/4 py-2 px-2 border border-gray-300'>¥{expense.amount}</td>
                   <td className='w-2/4 py-2 px-2 border border-gray-300 break-words'>{expense.description}</td>
@@ -166,9 +82,9 @@ export const MonthlyBudgetCalender: React.FC = () => {
               label='金額'
               name='amount'
               type='number'
-              value={storeExpenseInput.amount || ""}
-              onChange={setStoreExpenseTextInput}
-              validationErrorMessages={validationErrors.amount ?? []}
+              value={dialog.store.storeExpenseInput.amount || ""}
+              onChange={dialog.store.setStoreExpenseTextInput}
+              validationErrorMessages={dialog.store.validationErrors.amount ?? []}
             />
           </div>
           <div className='mb-2'>
@@ -177,9 +93,9 @@ export const MonthlyBudgetCalender: React.FC = () => {
               label='適用'
               name='description'
               type='text'
-              value={storeExpenseInput.description}
-              onChange={setStoreExpenseTextInput}
-              validationErrorMessages={validationErrors.description ?? []}
+              value={dialog.store.storeExpenseInput.description}
+              onChange={dialog.store.setStoreExpenseTextInput}
+              validationErrorMessages={dialog.store.validationErrors.description ?? []}
             />
           </div>
           <div className='mb-2'>
@@ -188,35 +104,19 @@ export const MonthlyBudgetCalender: React.FC = () => {
               label='カテゴリ'
               name='category'
               options={Object.entries(EXPENSE_CATEGORY)}
-              value={storeExpenseInput.category}
-              onChange={setStoreExpenseSelectInput}
-              validationErrorMessages={validationErrors.category ?? []}
+              value={dialog.store.storeExpenseInput.category}
+              onChange={dialog.store.setStoreExpenseSelectInput}
+              validationErrorMessages={dialog.store.validationErrors.category ?? []}
             />
           </div>
 
           <div className='w-full flex justify-center'>
-            <BaseButton borderColor='border-green-500' bgColor='bg-green-500' label='登録する' onClick={handleCreateExpense} />
+            <BaseButton borderColor='border-green-500' bgColor='bg-green-500' label='登録する' onClick={dialog.store.handleCreateExpense} />
           </div>
         </div>
       </div>
     </div>
   );
-
-  // NOTE: 表示中の月の収支集計
-  const summary = useMemo(() => {
-    let totalIncome = 0;
-    let totalExpense = 0;
-    if (!events) return { totalIncome, totalExpense };
-
-    for (const e of events) {
-      if (e.extendProps.type === "income") {
-        totalIncome += e.extendProps.totalAmount;
-      } else if (e.extendProps.type === "expense") {
-        totalExpense += e.extendProps.totalAmount;
-      }
-    }
-    return { totalIncome, totalExpense };
-  }, [events]);
 
   return (
     <div className='mx-auto mt-4'>
---
+++ frontend/features/monthly-budget/hooks/useMonthlyBudgetCalender.tsx
@@ -0,0 +1,137 @@
+import type { DatesSetArg } from "@fullcalendar/core/index.js";
+import type { DateClickArg } from "@fullcalendar/interaction/index.js";
+import { useCallback, useMemo, useState } from "react";
+import { getExpenses, getTotalAmounts, postCreateExpense } from "~/apis/expenses.api";
+import { getDateString } from "~/lib/date";
+import type { Expense, StoreExpenseInput, StoreExpenseValidationError, TotalAmountLists } from "~/types";
+
+const INITIAL_STORE_EXPENSE_INPUT = {
+  paidAt: new Date(),
+  amount: 0,
+  category: 0,
+  description: "",
+};
+const INITIAL_VALIDATION_ERRORS = {
+  amount: [],
+  category: [],
+  description: [],
+};
+
+export const useMonthlyBudgetCalender = () => {
+  const [events, setEvents] = useState<TotalAmountLists>([]);
+  const [inView, setInView] = useState(false);
+  const [selectedDate, setSelectedDate] = useState("");
+  const [selectedDateExpenses, setSelectedDateExpenses] = useState<Expense[]>([]);
+  const [currentMonthDate, setCurrentMonthDate] = useState<Date>(new Date());
+
+  const [storeExpenseInput, setStoreExpenseInput] = useState<StoreExpenseInput>(INITIAL_STORE_EXPENSE_INPUT);
+  const updateStoreExpenseInput = useCallback((params: Partial<StoreExpenseInput>) => {
+    setStoreExpenseInput((prev: StoreExpenseInput) => ({ ...prev, ...params }));
+  }, []);
+  const setStoreExpenseTextInput = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      updateStoreExpenseInput({ [e.target.name]: e.target.value });
+    },
+    [updateStoreExpenseInput],
+  );
+  const setStoreExpenseSelectInput = useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      updateStoreExpenseInput({ [e.target.name]: e.target.value });
+    },
+    [updateStoreExpenseInput],
+  );
+
+  const [validationErrors, setValidationErrors] = useState<StoreExpenseValidationError>(INITIAL_VALIDATION_ERRORS);
+
+  // NOTE: 月が変更された時の処理
+  const handleDatesSet = async (arg: DatesSetArg) => {
+    const selectedMonthBeginningDate = arg.view.currentStart;
+    const currentEnd = arg.view.currentEnd;
+    currentEnd.setDate(currentEnd.getDate() - 1);
+    const selectedMonthEndDate = currentEnd;
+
+    // TODO: ここをTanstack Query等を使用してキャッシュする
+    const fetchedTotalAmounts = await getTotalAmounts(getDateString(selectedMonthBeginningDate), getDateString(selectedMonthEndDate));
+    setCurrentMonthDate(selectedMonthBeginningDate);
+    setEvents(fetchedTotalAmounts);
+  };
+
+  // NOTE: 日が選択された時の処理
+  const handleDateClick = async (arg: DateClickArg) => {
+    // NOTE: 選択月以外の日付のクリックは無効にする
+    if (arg.date.getMonth() !== currentMonthDate.getMonth()) return;
+
+    const date = getDateString(arg.date);
+    setSelectedDateExpenses(await getExpenses(date, date));
+    setSelectedDate(date);
+    setInView(true);
+    setStoreExpenseInput((prev: StoreExpenseInput) => ({ ...prev, ...{ paidAt: arg.date } }));
+  };
+
+  const handleCreateExpense = async () => {
+    setValidationErrors(INITIAL_VALIDATION_ERRORS);
+
+    const { expense, errors } = await postCreateExpense(storeExpenseInput);
+    if (Object.keys(errors).length > 0) {
+      setValidationErrors(errors);
+      return;
+    }
+
+    window.alert("支出を登録しました");
+
+    // NOTE: 選択月の支出に反映する
+    const otherDateEvents = events.filter((event) => String(event.date) !== selectedDate);
+    const currentDateEvent = events.find((event) => String(event.date) === selectedDate);
+    const newTotalAmount = currentDateEvent?.extendProps.totalAmount ?? 0;
+    setEvents([
+      ...otherDateEvents,
+      {
+        date: expense.paidAt,
+        extendProps: {
+          type: "expense",
+          totalAmount: newTotalAmount + expense.amount,
+        },
+      },
+    ]);
+    setInView(false);
+    setStoreExpenseInput(INITIAL_STORE_EXPENSE_INPUT);
+  };
+
+  // NOTE: 表示中の月の収支集計
+  const summary = useMemo(() => {
+    let totalIncome = 0;
+    let totalExpense = 0;
+    if (!events) return { totalIncome, totalExpense };
+
+    for (const e of events) {
+      if (e.extendProps.type === "income") {
+        totalIncome += e.extendProps.totalAmount;
+      } else if (e.extendProps.type === "expense") {
+        totalExpense += e.extendProps.totalAmount;
+      }
+    }
+    return { totalIncome, totalExpense };
+  }, [events]);
+
+  return {
+    currentMonthDate,
+    summary,
+    handleDatesSet,
+    handleDateClick,
+    events,
+
+    dialog: {
+      inView,
+      setInView,
+      selectedDate,
+      selectedDateExpenses,
+      store: {
+        handleCreateExpense,
+        storeExpenseInput,
+        validationErrors,
+        setStoreExpenseTextInput,
+        setStoreExpenseSelectInput,
+      },
+    },
+  };
+};
