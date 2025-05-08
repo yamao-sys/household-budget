e81bf0bfd6f1c68a52b61c3d92cecdcd2ab442f4
yamao-sys <yamaopanku@gmail.com>
2025-05-08 22:26:17 +0900

    各ページにクライアントミドルウェアのcontextとuseAuth適用

--- frontend/app/monthly_budget/page.tsx
+++ frontend/app/monthly_budget/page.tsx
@@ -1,6 +1,20 @@
 import { MonthlyBudgetCalender } from "~/features/monthly-budget/components/Calender/MonthlyBudgetCalender";
+import type { Route } from "./+types/page";
+import { authContext } from "~/middlewares/auth-context";
+import { useLoaderData } from "react-router";
+import { useAuth } from "~/hooks/useAuth";
+
+export async function clientLoader({ context }: Route.ClientLoaderArgs) {
+  const auth = context.get(authContext);
+
+  return { isSignedIn: !!auth?.isSignedIn };
+}
 
 export default function MonthlyBudgetPage() {
+  const { isSignedIn } = useLoaderData<typeof clientLoader>();
+
+  useAuth(isSignedIn);
+
   return (
     <>
       <MonthlyBudgetCalender />
