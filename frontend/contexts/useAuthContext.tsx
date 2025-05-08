98558f65cffd9e3d4068cea1197dd22383fbe285
yamao-sys <yamaopanku@gmail.com>
2025-05-08 22:15:52 +0900

    AuthContextを作成

---
+++ frontend/contexts/useAuthContext.tsx
@@ -0,0 +1,27 @@
+import { createContext, type FC, useContext, useState } from "react";
+
+type AuthSetContextType = {
+  setAuth: React.Dispatch<React.SetStateAction<AuthContextType>>;
+};
+
+type AuthContextType = {
+  isSignedIn: boolean;
+};
+
+export const AuthContext = createContext<AuthContextType>({ isSignedIn: false });
+
+export const AuthSetContext = createContext<AuthSetContextType>({ setAuth: () => undefined });
+
+export const useAuthContext = () => useContext<AuthContextType>(AuthContext);
+
+export const useAuthSetContext = () => useContext<AuthSetContextType>(AuthSetContext);
+
+export const AuthProvider: FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [auth, setAuth] = useState<AuthContextType>({ isSignedIn: false });
+
+  return (
+    <AuthContext.Provider value={auth}>
+      <AuthSetContext.Provider value={{ setAuth }}>{children}</AuthSetContext.Provider>
+    </AuthContext.Provider>
+  );
+};
