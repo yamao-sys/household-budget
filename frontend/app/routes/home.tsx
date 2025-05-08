27f1f7754d2ce2e8c256b4305e024e457cd16ddb
yamao-sys <yamaopanku@gmail.com>
2025-05-09 02:36:31 +0900

    ログインリダイレクトはクライアントミドルウェアに任せているので変更

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,25 +1,31 @@
-import { getCheckSignedIn } from "~/apis/users.api";
+import { authContext } from "~/middlewares/auth-context";
 import type { Route } from "./+types/home";
+import { useLoaderData, useNavigate } from "react-router";
+import { useAuth } from "~/hooks/useAuth";
 import { useEffect } from "react";
-import { useNavigate } from "react-router";
 import { NAVIGATION_PAGE_LIST } from "../routes";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "New React Router App" }, { name: "description", content: "Welcome to React Router!" }];
 }
 
+export async function clientLoader({ context }: Route.ClientLoaderArgs) {
+  const auth = context.get(authContext);
+
+  return { isSignedIn: !!auth?.isSignedIn, csrfToken: auth?.csrfToken ?? "" };
+}
+
 export default function Home() {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    async function init() {
-      const isSignedIn = await getCheckSignedIn();
-      const toNavigatePath = isSignedIn ? NAVIGATION_PAGE_LIST.monthlyBudgetPage : NAVIGATION_PAGE_LIST.signInPage;
+  const { isSignedIn, csrfToken } = useLoaderData<typeof clientLoader>();
 
-      navigate(toNavigatePath);
-    }
-    init();
-  }, [navigate]);
+  useAuth(isSignedIn, csrfToken);
+
+  useEffect(() => {
+    const toNavigatePath = isSignedIn ? NAVIGATION_PAGE_LIST.monthlyBudgetPage : NAVIGATION_PAGE_LIST.signInPage;
+    navigate(toNavigatePath);
+  }, [isSignedIn]);
 
   return <></>;
 }
