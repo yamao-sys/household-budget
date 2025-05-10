010b260856fbd8dbc74c0abb95fd3deb15b7c773
yamao-sys <yamaopanku@gmail.com>
2025-05-10 13:18:15 +0900

    postCreateExpenseをuseMutateに適用

--- frontend/services/expenses/index.ts
+++ frontend/services/expenses/index.ts
@@ -1,6 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
+import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
 import { expenseKeys } from "./key";
-import { getExpenses } from "./api";
+import { getExpenses, postCreateExpense } from "./api";
+import type { StoreExpenseInput, StoreExpenseResponse } from "~/types";
 
 export const useGetExpenses = (fromDate: string, toDate: string, csrfToken: string) => {
   const { data, isPending, isError } = useQuery({
@@ -9,4 +10,24 @@ export const useGetExpenses = (fromDate: string, toDate: string, csrfToken: stri
   });
 
   return { data, isPending, isError };
+};
+
+export const usePostCreateExpense = (
+  queryClient: QueryClient,
+  onMutate: () => void,
+  onSuccess: (data: StoreExpenseResponse) => void,
+  input: StoreExpenseInput,
+  date: string,
+  csrfToken: string,
+) => {
+  return useMutation({
+    onMutate: () => onMutate,
+    mutationFn: () => postCreateExpense(input, csrfToken),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: expenseKeys.list(date, date),
+      });
+      onSuccess(data);
+    },
+  });
 };
