c5ccca2a70f1fdc1341d9d279a0eff65ce09ef3b
yamao-sys <yamaopanku@gmail.com>
2025-05-10 15:38:11 +0900

    postCreateExpense時にgetExpenseTotalAmountsのinvalidate

--- frontend/services/expenses/index.ts
+++ frontend/services/expenses/index.ts
@@ -2,6 +2,7 @@ import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
 import { expenseKeys } from "./key";
 import { getExpenses, getExpenseTotalAmounts, postCreateExpense } from "./api";
 import type { StoreExpenseInput, StoreExpenseResponse } from "~/types";
+import { getDateString } from "~/lib/date";
 
 export const useGetExpenses = (fromDate: string, toDate: string, csrfToken: string) => {
   const { data, isPending, isError } = useQuery({
@@ -33,9 +34,17 @@ export const usePostCreateExpense = (
     onMutate: () => onMutate,
     mutationFn: () => postCreateExpense(input, csrfToken),
     onSuccess: (data) => {
+      const selectedDate = new Date(date);
+      const beginningOfMonth = getDateString(new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1));
+      const endOfMonth = getDateString(new Date(selectedDate.getFullYear(), selectedDate.getMonth() + 1, 0));
+
       queryClient.invalidateQueries({
         queryKey: expenseKeys.list(date, date),
       });
+      queryClient.invalidateQueries({
+        queryKey: expenseKeys.totalAmount(beginningOfMonth, endOfMonth),
+      });
+
       onSuccess(data);
     },
   });
