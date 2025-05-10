9a0ff9152aa68b70f1e02d61c8975a519029812a
yamao-sys <yamaopanku@gmail.com>
2025-05-10 20:05:50 +0900

    CSRFトークン取得をservicesのもので

--- frontend/middlewares/auth-middleware.ts
+++ frontend/middlewares/auth-middleware.ts
@@ -1,8 +1,8 @@
 import { redirect, type unstable_MiddlewareFunction as MiddlewareFunction } from "react-router";
 import { authContext } from "./auth-context";
-import { getCsrfToken } from "~/apis/csrf.api";
 import { NAVIGATION_PAGE_LIST } from "~/app/routes";
 import { getCheckSignedIn } from "~/services/users/api";
+import { getCsrfToken } from "~/services/csrf/api";
 
 export const authMiddleware: MiddlewareFunction = async ({ request, context }) => {
   // NOTE: 画面遷移の際にCSRFトークンを取得
