fefaeae2a89efeb8f6571c75b7b32c0fb59f53bc
yamao-sys <yamaopanku@gmail.com>
2025-06-07 16:51:24 +0900

    IncomeリソースのTanstack QueryのキャッシュキーをOrval製のものに置き換え

--- frontend/services/incomes/index.ts
+++ frontend/services/incomes/index.ts
@@ -1,12 +1,12 @@
 import { QueryClient, useMutation, useQuery } from "@tanstack/react-query";
-import { incomeKeys } from "./key";
 import { getIncomeClientTotalAmounts, getIncomes, getIncomeTotalAmounts, postCreateIncome } from "./api";
 import { getDateString } from "~/lib/date";
 import type { StoreIncomeInput, StoreIncomeResponse } from "~/apis/model";
+import { getGetIncomesClientTotalAmountsQueryKey, getGetIncomesQueryKey, getGetIncomesTotalAmountsQueryKey } from "~/apis/incomes/incomes";
 
 export const useGetIncomes = (fromDate: string, toDate: string, csrfToken: string) => {
   const { data, isPending, isError } = useQuery({
-    queryKey: incomeKeys.list(fromDate, toDate),
+    queryKey: getGetIncomesQueryKey({ fromDate, toDate }),
     queryFn: () => getIncomes(fromDate, toDate, csrfToken),
   });
 
@@ -15,7 +15,7 @@ export const useGetIncomes = (fromDate: string, toDate: string, csrfToken: strin
 
 export const useGetIncomeTotalAmounts = (fromDate: string, toDate: string, csrfToken: string) => {
   const { data, isPending, isError } = useQuery({
-    queryKey: incomeKeys.totalAmount(fromDate, toDate),
+    queryKey: getGetIncomesTotalAmountsQueryKey({ fromDate, toDate }),
     queryFn: () => getIncomeTotalAmounts(fromDate, toDate, csrfToken),
     staleTime: 1000 * 60 * 10, // NOTE: FullCalenderで月を変更すると、キャッシュクリアされてしまうため設定
   });
@@ -25,7 +25,7 @@ export const useGetIncomeTotalAmounts = (fromDate: string, toDate: string, csrfT
 
 export const useGetIncomeClientTotalAmounts = (fromDate: string, toDate: string, csrfToken: string) => {
   const { data, isPending, isError } = useQuery({
-    queryKey: incomeKeys.clientTotalAmount(fromDate, toDate),
+    queryKey: getGetIncomesClientTotalAmountsQueryKey({ fromDate, toDate }),
     queryFn: () => getIncomeClientTotalAmounts(fromDate, toDate, csrfToken),
     staleTime: 1000 * 60 * 10,
   });
@@ -50,13 +50,13 @@ export const usePostCreateIncome = (
       const endOfMonth = getDateString(new Date(selectedDate.getFullYear(), selectedDate.getMonth() + 1, 0));
 
       queryClient.invalidateQueries({
-        queryKey: incomeKeys.list(date, date),
+        queryKey: getGetIncomesQueryKey({ fromDate: date, toDate: date }),
       });
       queryClient.invalidateQueries({
-        queryKey: incomeKeys.totalAmount(beginningOfMonth, endOfMonth),
+        queryKey: getGetIncomesTotalAmountsQueryKey({ fromDate: beginningOfMonth, toDate: endOfMonth }),
       });
       queryClient.invalidateQueries({
-        queryKey: incomeKeys.clientTotalAmount(beginningOfMonth, endOfMonth),
+        queryKey: getGetIncomesClientTotalAmountsQueryKey({ fromDate: beginningOfMonth, toDate: endOfMonth }),
       });
 
       onSuccess(data);
