2132a7a215bc78eeab81fca665468db8e6b2ee0d
yamao-sys <yamaopanku@gmail.com>
2025-06-07 12:14:57 +0900

    OrvalでbaseUrlに環境変数を使えるようにするため、vite.config.tsでprocess.envで環境変数を読めるように

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,17 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  server: {
-    host: "0.0.0.0",
-    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    server: {
+      host: "0.0.0.0",
+      port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    },
+    define: {
+      "process.env": loadEnv(mode, process.cwd()),
+    },
+  };
 });
